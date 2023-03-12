d328a379ea7276af0e51bedaa480bdb86bbb5bea
m-haketa <git@excelspeedup.com>
2023-03-12 19:26:30 +0900

    .d.tsを追加

---
+++ test/index.d.ts
@@ -0,0 +1,3 @@
+export type CompileContext = import('micromark-util-types').CompileContext
+export type HtmlOptions = import('../dev/index.js').HtmlOptions
+export type Handle = import('../dev/index.js').Handle
